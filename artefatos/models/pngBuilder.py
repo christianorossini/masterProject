5053e12184eb1cafda6a32943a6e49ebccdeee58
Christiano Rossini <christiano.rossini.mc@gmail.com>
2020-01-10 12:18:27 -0300

    commit

--- artefatos/models/pngBuilder.py
+++ artefatos/models/pngBuilder.py
@@ -3,9 +3,9 @@ import pydotplus
 from IPython.display import Image
 
 pyPath = os.path.dirname(os.path.abspath(__file__))
-path = "{0}/dt/escolhidos".format(pyPath)
+path = "{0}/filtered-dt".format(pyPath)
 
-files = ['gc']  
+files = ['lm']  
 for filename in files:
     dotPath = "{0}/{1}.dot".format(path, filename)
     graph = pydotplus.graph_from_dot_file(dotPath)
